5435494a5cf889e384cb67c809c65158adc19012
Neapolicy <matthewlin559@gmail.com>
2024-08-10 16:49:53 -0400

    stuff

--- app/page.js
+++ app/page.js
@@ -10,7 +10,48 @@ export default function Home() {
     }
   ]);
 
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState(''); //sets it to an empty string
+
+  const sendMessage = async () => {
+    setMessage('');
+    setMessages((messages) => [
+      ...messages, 
+      { role: 'user', content: message },
+      { role: 'assistant', content: '' }
+    ]);
+  
+    const response = await fetch('/api/chat', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify([...messages, { role: 'user', content: message }]),
+    }).then(async (res) =>{
+      const reader = res.body.getReader();
+      const decoder = new TextDecoder();
+    
+      let result = '';
+      return reader.read().then(function processText({ done, value }) {
+        if (done) {
+          return result;
+        }
+        const text = decoder.decode(value || new Int8Array(), { stream: true });
+        
+        setMessages((messages) => {
+          let lastMessage = messages[messages.length - 1];
+          let otherMessages = messages.slice(0, messages.length - 1); // gets all messages except the last
+          return [
+            ...otherMessages,
+            {
+              ...lastMessage,
+              content: lastMessage.content + text
+            },
+          ]
+        })
+        return reader.read().then(processText);
+    })
+    })    
+  };
 
   return (
     <Box 
@@ -57,12 +98,12 @@ export default function Home() {
         </Stack>
         <Stack direction={"row"} spacing={2}>
           <TextField
-          label = "message"
-          fullWidth
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+            label="Message"
+            fullWidth
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
-          <Button variant="contained">Send</Button>
+          <Button variant="contained" onClick={sendMessage}>Send</Button>
         </Stack>
       </Stack>
     </Box>
